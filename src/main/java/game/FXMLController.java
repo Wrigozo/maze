761b87f33709b86a37bf9f98535c6ce14c38d5d1
Wrigozo <szaboalexandra18@gmail.com>
2019-05-06 20:23:24 +0200

    add some changes

--- src/main/java/game/FXMLController.java
+++ src/main/java/game/FXMLController.java
@@ -1,14 +1,92 @@
 package game;
 
+import java.lang.reflect.Field;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 
 public class FXMLController {
     
     @FXML
     private Label label;
-    
+    @FXML
+    private Button btn00;
+    @FXML
+    private Button btn01;
+    @FXML
+    private Button btn02;
+    @FXML
+    private Button btn03;
+    @FXML
+    private Button btn04;
+    @FXML
+    private Button btn05;
+    @FXML
+    private Button btn10;
+    @FXML
+    private Button btn11;
+    @FXML
+    private Button btn12;
+    @FXML
+    private Button btn13;
+    @FXML
+    private Button btn14;
+    @FXML
+    private Button btn15;
+    @FXML
+    private Button btn20;
+    @FXML
+    private Button btn21;
+    @FXML
+    private Button btn22;
+    @FXML
+    private Button btn23;
+    @FXML
+    private Button btn24;
+    @FXML
+    private Button btn25;
+    @FXML
+    private Button btn30;
+    @FXML
+    private Button btn31;
+    @FXML
+    private Button btn32;
+    @FXML
+    private Button btn33;
+    @FXML
+    private Button btn34;
+    @FXML
+    private Button btn35;
+    @FXML
+    private Button btn40;
+    @FXML
+    private Button btn41;
+    @FXML
+    private Button btn42;
+    @FXML
+    private Button btn43;
+    @FXML
+    private Button btn44;
+    @FXML
+    private Button btn45;
+    @FXML
+    private Button btn50;
+    @FXML
+    private Button btn51;
+    @FXML
+    private Button btn52;
+    @FXML
+    private Button btn53;
+    @FXML
+    private Button btn54;
+    @FXML
+    private Button btn55;
+
+    private Button [][] btns=new Button[6][6];
+
+
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
@@ -17,5 +95,26 @@ public class FXMLController {
     
     public void initialize() {
         // TODO
+
+        for(int i=0; i<6; i++){
+            for (int j=0; j<6; j++){
+                try
+                {
+                    Field f = this.getClass().getDeclaredField("btn" + Integer.toString(i) + Integer.toString(j));
+                    btns[i][j]=(Button)f.get(this);
+
+                }
+                catch (NoSuchFieldException ex)
+                {
+                    System.out.println("no such field");
+
+                }
+                catch (IllegalAccessException ex2)
+                {
+                    System.out.println("illegal access");
+                }
+
+            }
+        }
     }    
 }
