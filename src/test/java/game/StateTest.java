f0b9ec617ff7b3313f3e8cb7fd2be9b68eea479b
Wrigozo <szaboalexandra18@gmail.com>
2019-05-16 11:25:20 +0200

    StateTest changed to public

--- src/test/java/game/StateTest.java
+++ src/test/java/game/StateTest.java
@@ -1,5 +1,27 @@
+package game;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class StateTest {
+public class StateTest {
+    State state=new State(0,0);
+    int []testArray={0,0};
 
+    @Test
+    void getEnableButtonsPlayer() {
+    }
+
+    @Test
+    void getEnableButtonsEnemy() {
+    }
+
+    @Test
+    void enemylepes() {
+        assertArrayEquals(testArray, state.enemylepes(0,0,0,0));
+    }
+
+    @Test
+    void setEnableButtons() {
+    }
 }
