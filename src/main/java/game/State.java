9d3b26a8503c9b75252dce9c8694121327fda1ff
Wrigozo <szaboalexandra18@gmail.com>
2019-05-16 13:10:35 +0200

    add some changes to README.md

--- src/main/java/game/State.java
+++ src/main/java/game/State.java
@@ -6,9 +6,16 @@ import java.util.ArrayList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ *vdfs
+ */
 public class State {
 
     //1.fel 2.jobbra 3.le 4.balra
+    /**
+     * Egy {@code int[][][]} tömb, mely labirintus mátrixának megfelelő koordinátáin tárolja, hogy hol vannak falak, a
+     * következő sorrendben: fent, jobbra, lent és balra. Az 0-ás érték jelentése, hogy van fal, a 1-es pedig, hogy nincs.
+     */
     public static int[][][] canStep = {
             {{0, 1, 0, 0}, {0, 1, 1, 1}, {0, 1, 1, 1}, {0, 1, 1, 1}, {0, 0, 1, 1}, {0, 0, 1, 0}},
             {{0, 1, 1, 0}, {1, 0, 0, 1}, {1, 1, 1, 0}, {1, 0, 1, 1}, {1, 0, 0, 0}, {1, 0, 1, 0}},
@@ -18,17 +25,52 @@ public class State {
             {{1, 1, 0, 0}, {1, 1, 0, 1}, {1, 1, 0, 1}, {1, 1, 0, 1}, {1, 1, 0, 1}, {1, 0, 0, 1}}
     };
     private static Logger logger = LoggerFactory.getLogger(State.class);
-    //a labda helyzete
+
+    /**
+     * Egy {@code int} érték, játékos x koordinátáját tárolja..
+     */
     public int actualPosX;
+    /**
+     * Egy {@code int} érték, játékos y koordinátáját tárolja..
+     */
     public int actualPosY;
+    /**
+     * Egy {@code int[]} érték, mely a szörny lehetséges lépéseit tárolja a szörny koordinátáinak megfelelően,
+     * a következő sorrendben fent, jobbra, lent és balra. Az 1-es érték jelentése, hogy léphet a megfelelő irányba,
+     * a 0-ás pedig, hogy nem.
+     */
     public int[] lepesekEnemy = new int[4];
-    public List<String> ishorizontalPlayer;
-    public List<String> isVerticalPlayer;
-    public List<String> ishorizontalEnemy;
-    public List<String> isVerticalEnemy;
-    public List<String> enableButtonsPlayer = new ArrayList<>();
-    public List<String> enableButtonsEnemy = new ArrayList<>();
+    /**
+     * Egy {@code int[]} érték, mely a játékos lehetséges lépéseit tárolja a játékos koordinátáinak megfelelően,
+     * a következő sorrendben fent, jobbra, lent és balra. Az 1-es érték jelentése, hogy léphet a megfelelő irányba,
+     * a 0-ás pedig, hogy nem.
+     */
     private int[] lepesekPlayer;
+    /**
+     * Egy {@link List}&lt;{@link String}&gt; típusú objektum, mely azoknak a gomboknak a nevét tárolja, ahova a játékos vízszintes irányú lépéssel léphet.
+     */
+    public List<String> ishorizontalPlayer;
+    /**
+     * Egy {@link List}&lt;{@link String}&gt; típusú objektum, mely azoknak a gomboknak a nevét tárolja, ahova a játékos függőleges irányú lépéssel léphet.
+     */
+    public List<String> isVerticalPlayer;
+    /**
+     * Egy {@link List}&lt;{@link String}&gt; típusú objektum, mely azoknak a gomboknak a nevét tárolja, ahova a szörny vízszintes irányú lépéssel léphet.
+     */
+    public List<String> ishorizontalEnemy;
+    /**
+     * Egy {@link List}&lt;{@link String}&gt; típusú objektum, mely azoknak a gomboknak a nevét tárolja, ahova a szörny függőleges irányú lépéssel léphet.
+     */
+    public List<String> isVerticalEnemy;
+    /**
+     * Egy {@link List}&lt;{@link String}&gt; típusú objektum, mely tárolja azoknak a gomboknak a nevét,hogy ahova léphet a játékos.
+     */
+    public List<String> enableButtonsPlayer = new ArrayList<>();
+    /**
+     * Egy {@link List}&lt;{@link String}&gt; típusú objektum, mely tárolja azoknak a gomboknak a nevét,hogy ahova léphet a szörny.
+     */
+    public List<String> enableButtonsEnemy = new ArrayList<>();
+
 
 
     public State(int actualposX, int actualposY) {
@@ -44,6 +86,7 @@ public class State {
     public List<String> getEnableButtonsEnemy() {
         return enableButtonsEnemy;
     }
+
 
     public int[] enemylepes(int enemyPosX, int enemyPosY, int playerPosX, int playerPosY) {
         int[] s = new int[2];
@@ -62,6 +105,7 @@ public class State {
         this.isVerticalPlayer = new ArrayList<>();
         EnableButtons(actualX, actualY, ishorizontalPlayer, isVerticalPlayer, state.lepesekPlayer, enableButtonsPlayer);
     }
+
 
     private void EnableButtons(int posX, int posY, List<String> isHorizontal, List<String> isVertical, int[] lepesek, List<String> enableButtons) {
         int tmpX = posX;
@@ -92,6 +136,7 @@ public class State {
             }
         }
     }
+
 
     private void checkDirection(int enemyPosX, int enemyPosY, int playerPosX, int playerPosY, int[] s, List<String> isHorizontal, List<String> isVertical, int[] lepesek) {
         if (isHorizontal.size() != 0) {
