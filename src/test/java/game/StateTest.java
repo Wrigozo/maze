e322e597e8daf4c85b8ccac575b86ced8534f69a
Wrigozo <szaboalexandra18@gmail.com>
2019-05-16 10:53:42 +0200

    added README.md

--- src/test/java/game/StateTest.java
+++ src/test/java/game/StateTest.java
@@ -1,44 +1,5 @@
-package game;
-
-import org.apache.logging.log4j.core.util.Assert;
-import org.junit.jupiter.api.Test;
-import java.util.ArrayList;
-import java.util.List;
-import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.params.ParameterizedTest;
+class StateTest {
 
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-
-import static org.junit.jupiter.params.provider.Arguments.*;
-
-public class StateTest {
-    State state=new State();
-
-
-    @Test
-    public void fel() {
-    }
-
-    @Test
-    public void jobbra() {
-    }
-
-    @Test
-    public void le() {
-    }
-
-    @Test
-    public void balra() {
-    }
-
-    @Test
-    public void getEnableButtons() {
-    }
-
-    @Test
-    public void setEnableButtons() {
-    }
 }
